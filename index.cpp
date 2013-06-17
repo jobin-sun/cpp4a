3cc400bf92e01bfaf8e3020eec09d27e7ef71c06
panda110 <1149225004@qq.com>
2013-06-17 22:05:32 +0800

    a bit motify

--- index.cpp
+++ index.cpp
@@ -1,5 +1,10 @@
-extern "C" char* run();
-char* run(){
-	char test[]="Hello2";
-	return test;	
+#include "../include/httpd.h"
+extern "C" int CPP4A_RUN(request_rec,char**);
+extern "C" int CPP4A_CLOSE();
+int CPP4A_RUN(request_rec r,char** chRst){
+	char* test="你好";
+	*chRst=test;
+	return OK;	
+}
+int CPP4A_CLOSE(){
 }
